f8441489a20df0c7f4424fc8e695db8d500ff489
Naeem-ur-rahman <naeemur1001@gmail.com>
2024-10-14 01:32:34 +0500

    create client pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
