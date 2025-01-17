c18f4e81d68fbd707148a4796527777a56177a33
Naeem-ur-rahman <naeemur1001@gmail.com>
2025-01-17 19:58:53 +0500

    Added the image upload half functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,30 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 const Profile = () => {
     const { currentUser } = useSelector(state => state.user)
+    const fileRef = useRef(null);
+    const [file, setFile] = useState(undefined);
+
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
+
+    const handleFileUpload = async (file) => {
+        console.log(file)
+        //need to do that using the node server using multer.
+    }
+    
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center m-7">Profile</h1>
             <form className="flex flex-col gap-4">
+                <input type="file" onChange={(e) => setFile(e.target.files[0])} ref={fileRef} hidden accept="image/*" />
                 <img src={currentUser.avatar}
                     alt="profile-image"
                     className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+                    onClick={() => fileRef.current.click()}
                 />
                 <input type="text"
                     placeholder="username"
